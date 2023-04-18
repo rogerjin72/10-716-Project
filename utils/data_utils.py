40d4c522440454ccdffa554f7bd0282fc15382c3
Roger Jin <rogerjin@Rogers-MacBook-Pro.local>
2023-04-18 17:32:13 -0400

    edits to data_utils

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,6 +4,7 @@ import torchvision
 from utils.config import *
 from torch.utils.data import DataLoader
 
+
 def get_mnist(fp, train=True, batch_size=64):
     mnist_transforms = torchvision.transforms.Compose(
         [
@@ -11,6 +12,6 @@ def get_mnist(fp, train=True, batch_size=64):
             torchvision.transforms.Normalize((0.1307,), (0.3081,)),
         ]
     )    
-
+    
     dataset = torchvision.datasets.MNIST(fp, train, download=True, transform=mnist_transforms)
     return DataLoader(dataset, batch_size, shuffle=not train)
